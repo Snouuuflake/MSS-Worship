fa9f5cbab0e55709777edfccb0cbc4d1a1935456
Snouuuflake <xuehuamusic2@gmail.com>
2024-01-12 16:43:42 -0600

    gonna mess w styles

--- src/display.js
+++ src/display.js
@@ -1,12 +1,45 @@
-displayDiv = document.querySelector(".display-global");
-body = document.querySelector("body");
+const displayDiv = document.querySelector(".display-global-text");
+const displayContainer = document.querySelector(".display-container");
+const body = document.querySelector("body");
 
 
-window.displayAPI.onReadyFromMain( (value) => {
+
+function overflows(element) {
+  return (element.clientHeight < element.scrollHeight);
+}
+
+function fitText(textElement, parentElement, maxSize) {
+  let i = 0;
+  textElement.style.fontSize = i + 'px';
+
+  for (i = 0; !overflows(parentElement) && (i != maxSize); i++) {
+    textElement.style.fontSize = i + 'px';
+    console.log(i);
+  }
+
+  if (overflows(parentElement)) {
+    textElement.style.fontSize = (i - 2) + 'px';
+    console.log(i);
+  }
+}
+
+let maxFontSize = 0;
+
+
+
+window.displayAPI.onReadyFromMain( (value) => { 
   displayDiv.classList.add("display-" + value + "-text");
   body.classList.add("display-" + value + "-background");
+
+  maxFontSize = parseInt(getComputedStyle(displayDiv).fontSize);
 });
 
 window.displayAPI.onGetDisplayText( (value) => {
   displayDiv.innerHTML = value;
+  fitText(displayDiv, displayContainer, maxFontSize);
 })
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -181,17 +181,21 @@ app.on('ready', () => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
+//
+// I dont think this makes sense here
+/*
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+*/
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 });
 
