67a7288228a8f409141f7824c3d40d92fd807365
Snouuuflake <xuehuamusic2@gmail.com>
2024-05-20 19:21:18 -0600

    dupe and del buttons work in terms of editing the song object correctly

--- src/editor.js
+++ src/editor.js
@@ -1,5 +1,3 @@
-const sectionArray = []
-
 const bigInput = document.getElementById("bigInput");
 const addSectionButton = document.getElementById("addSection");
 const sectionButtonContainer = document.getElementById("sectionButtonContainer");
@@ -12,16 +10,24 @@ bigInput.addEventListener("input", (event) => {
   console.log(bigInput.value);
 });
 
+/**
+ * Swaps two array indexes
+ */
 function swapElements(array, index1, index2) {
   [array[index1], array[index2]] = [array[index2], array[index1]];
 }
 
-function drawSectionButton(section, index) {
+/**
+ * Draws a section button
+ *
+ * @param index Allows button's event listeners to know its index
+ */
+function drawSectionButton(name, index) {
   const newBox = document.createElement("div");
   newBox.classList.add("sectionButtonBox");
 
   const nameSpan = document.createElement("span");
-  nameSpan.innerText = section.name;
+  nameSpan.innerText = name;
   nameSpan.classList.add("sectionNameSpan");
 
   const delButton = document.createElement("button");
@@ -29,10 +35,26 @@ function drawSectionButton(section, index) {
   delButton.innerText = "X";
   delButton.classList.add("sectionSubButton");
 
+  delButton.addEventListener("click", () => {
+    if (lSong.sectionOrder.filter((n) => { return n == name }).length == 1) {
+      // TODO: add confirmation buttons prompt for this case
+      lSong.sectionOrder = lSong.sectionOrder.filter((n) => { return n != name })
+      lSong.sections = lSong.sections.filter((s) => { return s.name != name })
+    } else {
+      lSong.sectionOrder.splice(index, 1);
+    }
+    drawSectionArr();
+  });
+
   const dupeButton = document.createElement("button");
   dupeButton.type = "button";
   dupeButton.innerText = "📄";
   dupeButton.classList.add("sectionSubButton");
+
+  dupeButton.addEventListener("click", () => {
+    lSong.sectionOrder.push(name);
+    drawSectionArr();
+  });
 
   const upButton = document.createElement("button");
   upButton.type = "button";
@@ -40,8 +62,8 @@ function drawSectionButton(section, index) {
   upButton.classList.add("sectionSubButton");
 
   upButton.addEventListener("click", () => {
-    if (index > 0 && sectionArray.length > 1) {
-      swapElements(sectionArray, index, (index - 1));
+    if (index > 0 && lSong.sectionOrder.length > 1) {
+      swapElements(lSong.sectionOrder, index, (index - 1));
       drawSectionArr();
     }
   });
@@ -52,38 +74,57 @@ function drawSectionButton(section, index) {
   downButton.classList.add("sectionSubButton");
 
   downButton.addEventListener("click", () => {
-    if (index < (sectionArray.length - 1) && sectionArray.length > 1) {
-      swapElements(sectionArray, index, (index + 1));
+    if (index < (lSong.sectionOrder.length - 1) && lSong.sectionOrder.length > 1) {
+      swapElements(lSong.sectionOrder, index, (index + 1));
       drawSectionArr();
     }
   });
 
   newBox.appendChild(nameSpan);
   newBox.appendChild(dupeButton);
+  newBox.appendChild(delButton);
   newBox.appendChild(upButton);
   newBox.appendChild(downButton);
 
   sectionButtonContainer.appendChild(newBox);
 }
 
+/**
+ * Draws all section buttons in global lSong.sectionOrder
+ */
 function drawSectionArr() {
   sectionButtonContainer.innerHTML = "";
   let i = 0;
-  for (let s of sectionArray) {
-    drawSectionButton(s, i);
+  for (let name of lSong.sectionOrder) {
+    drawSectionButton(name, i);
     i++;
   }
 }
 
+/**
+ * Adds a new section to global sectionArray
+ * WARN: does not check for name duplicates !
+ *
+ * @param name Name of the new section
+ */
 function pushSection(name) {
   const newSection = window.mss.Section();
   newSection.name = name;
 
-  sectionArray.push(newSection);
+  lSong.sections.push(newSection);
+  lSong.sectionOrder.push(name)
 
   drawSectionArr();
 }
 
+/**
+ * My async and bad version of the prompt method
+ *
+ * @param heading Prompt text
+ * @param filler Default value for input box
+ *
+ * @return null if exited or input box contents
+ */
 async function prompt2(heading, filler) {
   const promptBox = document.createElement("div");
   promptBox.classList.add("promptBox");
@@ -136,7 +177,7 @@ addSectionButton.addEventListener("click", (event) => {
   console.log("clicked!");
 
   prompt2("hi!", "filler").then((result) => {
-    if (result == null || result == "" || sectionArray.find((a) => { return a.name == result })) { // validates that a section with that name doesnt already exist
+    if (result == null || result == "" || lSong.sectionOrder.find((a) => { return a == result })) { // validates that a section with that name doesnt already exist
       console.log("No new section name!");
     } else {
       console.log("New section name: " + result);
